08a0aba42d2c0249f2638f244182a0407456380e
AnjaneshwarRao <89423692+AnjaneshwarRao@users.noreply.github.com>
2024-01-07 18:36:11 -0600

    Added another main class file

---
+++ src/main/java/com/example/demo/class_main.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class class_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("the second main class");
+	}
+
+}
--- src/main/java/com/example/demo/example_main.java
+++ src/main/java/com/example/demo/example_main.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class example_main {
 	public static void main(String[]args) {
 		System.out.println("the value is getting printed");
+		System.out.println("the first commit is done");
 	}
 
 }
